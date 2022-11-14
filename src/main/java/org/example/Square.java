70f6db3b417f6899cd173a6ddf6fad585bac8d29
Luke Moon <lukemoon88@yahoo.co.uk>
2022-11-14 11:37:44 +0000

    square class completed

--- src/main/java/org/example/Square.java
+++ src/main/java/org/example/Square.java
@@ -1,4 +1,31 @@
 package org.example;
 
 public class Square {
+    boolean mined;
+    int proximity;
+    boolean uncovered;
+    boolean flagged;
+
+    public Square(boolean m, int prox, boolean unc, boolean fl) {
+        this.mined = m;
+        this.proximity = prox;
+        this.uncovered = unc;
+        this.flagged = fl;
+
+    }
+
+    // Getter methods
+    public boolean getMined() {
+        return this.mined;
+    }
+    public int getProximity() {
+        return this.proximity;
+    }
+    public boolean getUncovered() {
+        return this.uncovered;
+    }
+
+    public boolean isFlagged() {
+        return this.flagged;
+    }
 }
