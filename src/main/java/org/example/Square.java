27de9fc7aae9e3765d3c4e84aa70c2610380ad02
Luke Moon <lukemoon88@yahoo.co.uk>
2022-11-15 09:58:33 +0000

    added code to square to contain own coord data

--- src/main/java/org/example/Square.java
+++ src/main/java/org/example/Square.java
@@ -7,12 +7,17 @@ public class Square {
     boolean flagged;
     int adjacent_mines;
 
+    int x_coord;
+    int y_coord;
+
     public Square() {
         this.mined = false;
         this.proximity = 0;
         this.uncovered = false;
         this.flagged = false;
         this.adjacent_mines = 0;
+        this.x_coord = 0;
+        this.y_coord = 0;
 
     }
 
@@ -33,6 +38,13 @@ public class Square {
         return this.adjacent_mines;
     }
 
+    public int getX_coord() {
+        return this.x_coord
+    }
+
+    public int getY_coord() {
+        return this.y_coord;
+    }
 
     // Setter methods
     public void setMined(boolean mined) {
@@ -53,5 +65,13 @@ public class Square {
 
     public void setAdjMines(int adj_mines) {
         this.adjacent_mines = adj_mines;
+    }
+
+    public void setX_coord(int x_coord) {
+        this.x_coord = x_coord;
+    }
+
+    public void setY_coord(int y_coord) {
+        this.y_coord = y_coord;
     }
 }
